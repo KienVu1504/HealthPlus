3a57f03b3e56f02d516bee603f390c31de3540fb
Kien Vu <kvstudio.154@gmail.com>
2022-08-10 15:14:51 +0700

    update data

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,15 @@
+$(function() {
+	$('#NameForm').submit(function() {
+		username = $('#user').val()
+		localStorage.setItem('username', username)
+    })
+});
+
+// $(window).on("load",function(){
+username = localStorage.getItem('username');
+if (username == null || username.length == 0) {
+    $('#nameShow').html('Hello User');
+} else {
+    $('#nameShow').html('Hello: ' + username);
+}
+// });
