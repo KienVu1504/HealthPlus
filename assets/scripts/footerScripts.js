82b8ddf7bac8a8de266aa48df335274bc866939b
Kien Vu <kvstudio.154@gmail.com>
2022-08-11 20:24:23 +0700

    fixing some bugs

---
+++ assets/scripts/footerScripts.js
@@ -0,0 +1,62 @@
+//realtime clock
+function refreshTime() {
+    const timeDisplay = document.getElementById("time");
+    const dateString = new Date().toLocaleString();
+    const formattedString = dateString.replace(", ", " - ");
+    timeDisplay.textContent = formattedString;
+}
+
+setInterval(refreshTime, 1000);
+
+//get location via lat & lon
+$(function() {
+    const button = document.getElementById("geoLocation");
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }else{
+        button.innerText = "Your browser not support";
+    }
+
+    function onSuccess(position){
+        button.innerText = "Detecting your location...";
+        let {latitude, longitude} = position.coords;
+        fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=f7473864d3c34b499d1bd69b635decd5`)
+        .then(response => response.json()).then(response =>{
+            let allDetails = response.results[0].components;
+            console.table(allDetails);
+            let {county, postcode, country} = allDetails;
+            button.innerText = `${country}`;
+        }).catch(()=>{
+            button.innerText = "Something went wrong";
+        });
+    }
+    
+    function onError(error){
+        if(error.code == 1){
+            button.innerText = "You denied the request";
+        }else if(error.code == 2){
+            button.innerText = "Location is unavailable";
+        }else{
+            button.innerText = "Something went wrong";
+        }
+
+        button.setAttribute("disabled", "true");
+    }
+});
+
+//visiter counter
+var n = localStorage.getItem('on_load_counter');
+
+if (n === null) {
+  n = 0;
+}
+n++;
+
+localStorage.setItem("on_load_counter", n);
+
+nums = n.toString().split('').map(Number);
+document.getElementById('CounterVisitor').innerHTML = '';
+
+for (var i of nums) {
+  document.getElementById('CounterVisitor').innerHTML += '<span class="counter-item">' + i + '</span>';
+}
--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,3 +1,4 @@
+//save user name
 $(function() {
 	$('#NameForm').submit(function() {
 		username = $('#user').val()
@@ -5,65 +6,10 @@ $(function() {
     })
 });
 
+//get and show user name 
 username = localStorage.getItem('username');
 if (username == null || username.length == 0) {
     $('#nameShow').html('Hello User');
 } else {
     $('#nameShow').html('Hello: ' + username);
-}
-
-function refreshTime() {
-    const timeDisplay = document.getElementById("time");
-    const dateString = new Date().toLocaleString();
-    const formattedString = dateString.replace(", ", " - ");
-    timeDisplay.textContent = formattedString;
-  }
-    setInterval(refreshTime, 1000);
-
-$(function() {
-    const button = document.getElementById("geoLocation");
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    }else{
-        button.innerText = "Your browser not support";
-    }
-
-    function onSuccess(position){
-        button.innerText = "Detecting your location...";
-        let {latitude, longitude} = position.coords;
-        fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=f7473864d3c34b499d1bd69b635decd5`)
-        .then(response => response.json()).then(response =>{
-            let allDetails = response.results[0].components;
-            console.table(allDetails);
-            let {county, postcode, country} = allDetails;
-            button.innerText = `${country}`;
-        }).catch(()=>{
-            button.innerText = "Something went wrong";
-        });
-    }
-    function onError(error){
-        if(error.code == 1){
-            button.innerText = "You denied the request";
-        }else if(error.code == 2){
-            button.innerText = "Location is unavailable";
-        }else{
-            button.innerText = "Something went wrong";
-        }
-        button.setAttribute("disabled", "true");
-    }
-});
-
-var n = localStorage.getItem('on_load_counter');
-
-if (n === null) {
-  n = 0;
-}
-n++;
-
-localStorage.setItem("on_load_counter", n);
-
-nums = n.toString().split('').map(Number);
-document.getElementById('CounterVisitor').innerHTML = '';
-for (var i of nums) {
-  document.getElementById('CounterVisitor').innerHTML += '<span class="counter-item">' + i + '</span>';
 }
